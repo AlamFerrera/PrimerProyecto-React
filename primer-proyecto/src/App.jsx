cd734e2be1707170b4b7a04cb4b3dd06a4ca2d3c
AlamFerrera <38860195+AlamFerrera@users.noreply.github.com>
2021-06-09 13:13:06 -0400

    Primer Commit
    Primer Commit React

---
+++ primer-proyecto/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Hola from './Components/hola.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Hola></Hola>
+    </div>
+  );
+}
+
+export default App;
---
+++ primer-proyecto/src/Components/hola.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Hola()
+{
+    let nombre = "Manuel";
+    let edad = 22;
+    return(
+        <div>
+            <h2>Hola {nombre} tu edad es {edad}</h2>
+        </div>
+    );
+}
+
+export default Hola;
